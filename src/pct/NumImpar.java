4ef3d1009d922a079abe0503c391aa88da3a5146
Denis Eduardo <89614090+iamghost99@users.noreply.github.com>
2021-10-28 22:15:28 -0200

    Projeto print de numeros impares

--- src/pct/NumImpar.java
+++ src/pct/NumImpar.java
@@ -14,21 +14,45 @@ import java.util.Scanner;
 public class NumImpar {
     public static void main(String[] args) {
         //Declaração de variáveis
-        int n;
-        
+        int limite;        
         //Objeto de entrada
-        Scanner entrada = new Scanner(System.in);
-        
+        Scanner entrada = new Scanner(System.in);        
         //Apresentação
-        System.out.println("\n\t\t\t -- Números ìmpares de 10 até 0 -- \n");
-        
+        System.out.println("\n\n\t-- Só numeros impares !! -- \n");
+        System.out.print("\t\t\t Digite o valor limite (10, 100, 1000, 10000): ");
+        limite = entrada.nextInt();        
         //Processamento e saída
-        for(int i = 10; i >= 0; i--){
-            if((i % 2) != 0)
-                System.out.printf(" %d ", i);
-        }
-        System.out.println("\n\n");
-        
-    }
-    
+        switch (limite) {
+            case 10:
+                for(int i = 0; i <= 10; i++){
+                    if((i % 2) != 0)
+                        System.out.println(i);
+                }
+                System.out.println("\n\n");
+                break;
+            case 100:
+                for(int i = 0; i <= 100; i++){
+                    if((i % 2) != 0)
+                        System.out.println(i);
+                }
+                System.out.println("\n\n");
+                break;
+            case 1000:
+                for(int i = 0; i <= 1000; i++){
+                    if((i % 2) != 0)
+                        System.out.println(i);
+                }
+                System.out.println("\n\n");
+                break;
+            case 10000:
+                for(int i = 0; i <= 10000; i++){
+                    if((i % 2) != 0)
+                        System.out.println(i);
+                }
+                System.out.println("\n\n");
+                break;
+            default:
+                System.out.println("\t\t\t Entrada incorreta ....");
+        }       
+    }   
 }
